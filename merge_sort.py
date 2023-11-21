0d9d93f70541137e4f46c8459a0022154f42ff11
ibrahemassa <oioi.ll58@outlook.com>
2023-11-21 18:14:12 +0300

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,25 +1,22 @@
 def merge_sort(arr):
     if len(arr) <= 1:
         return
-    l, r = 0, len(arr)
-    m = (l+r)//2
-    # print(f'Splitting: {arr}')
-    left, right = arr[l:m], arr[m:r]
+    m = len(arr)//2
+    left, right = arr[:m], arr[m:]
     merge_sort(left)
     merge_sort(right)
     merge(left, right, arr)
 
+
 def merge(left, right, arr):
-    i, l, r =0, 0, 0
+    i = l = r = 0
     while l < len(left) and r < len(right):
         if left[l] < right[r]:
-            # print(f'merging: {left[l]}')
             arr[i] = left[l]
             l += 1
             i += 1
         else:
             arr[i] = right[r]
-            # print(f'merging: {right[r]}')
             r += 1
             i += 1
     while l < len(left):
@@ -32,6 +29,6 @@ def merge(left, right, arr):
             i += 1
 
 
-x = [356, 97, 846, 215]
+x = [356, 97, 846, 215, 213, 301]
 merge_sort(x)
 print(x)
