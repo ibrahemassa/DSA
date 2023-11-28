dc6e17e3b00bfc8263a4872c6d07b1dbcdd86fb0
ibrahemassa <oioi.ll58@outlook.com>
2023-11-28 07:25:04 +0300

    Add selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,15 @@
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_num, min_index = float('inf'), i
+
+        for j in range(i, len(arr)):
+            if arr[j] < min_num:
+                min_num, min_index = arr[j], j
+
+        arr[i], arr[min_index] = min_num, arr[i]
+
+
+x = [7, 3, 4, 1, 8, 5, 99, 0, 4]
+print(f'Unsorted list: {x}')
+selection_sort(x)
+print(f'Sorted list: {x}')
